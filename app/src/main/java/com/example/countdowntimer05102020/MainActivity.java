2e79b7f2f96b49b281b9a5ffdd009e3926834f02
phatdroid94 <phatdroid94@gmail.com>
2020-10-26 20:55:08 +0700

    hien thi hinh

--- app/src/main/java/com/example/countdowntimer05102020/MainActivity.java
+++ app/src/main/java/com/example/countdowntimer05102020/MainActivity.java
@@ -2,32 +2,59 @@ package com.example.countdowntimer05102020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.concurrent.CountDownLatch;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView mImgBanner;
+    Button mBtnStart;
+    int[] mArrayImages = {
+            R.drawable.hinh1,
+            R.drawable.hinh2,
+            R.drawable.hinh3,
+            R.drawable.hinh4,
+            R.drawable.hinh5,
+    };
+    int mCount = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CountDownTimer countDownTimer = new CountDownTimer(2040, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                if ((millisUntilFinished / 1000) > 0) {
-                    Toast.makeText(MainActivity.this, (millisUntilFinished / 1000) + "", Toast.LENGTH_SHORT).show();
-                }
-            }
 
+        mImgBanner = findViewById(R.id.imageView);
+        mBtnStart = findViewById(R.id.buttonStart);
+
+        mBtnStart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFinish() {
-                Toast.makeText(MainActivity.this, "Ket thuc", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                CountDownTimer countDownTimer = new CountDownTimer() {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        if (mCount >= mArrayImages.length) {
+                            mCount = 0;
+                        }
+                        mImgBanner.setImageResource(mArrayImages[mCount++]);
+                    }
+
+                    @Override
+                    public void onFinish() {
+
+                    }
+                };
+
             }
-        };
-        countDownTimer.start();
+        });
+
+
+
     }
 }
